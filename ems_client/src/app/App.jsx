215e9ed12a296f50080e99cb9aee258dcab7ece1
sakethbikkina07 <sakethbikkina@gmail.com>
2026-08-15 19:33:52 +0530

    Update App.jsx

--- ems_client/src/app/App.jsx
+++ ems_client/src/app/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import Login from '../features/auth/pages/Login';
 import Register from '../features/auth/pages/Register';
 import Sidebar from '../components/Organisms/SideBar';
-
 function App() {
   return (
     <div>
