40e5c05d922d5713e1e16fa891d9cb62381ce2b4
sakethbikkina07 <sakethbikkina@gmail.com>
2026-08-18 23:09:14 +0530

    updated app.jsx and main.jsx routes

--- ems_client/src/main.jsx
+++ ems_client/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from '../src/app/App'
 import { ThemeProvider } from './Contexts/ThemeContext'
 import { BrowserRouter } from 'react-router-dom'
