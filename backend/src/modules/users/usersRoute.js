a236cb558a2542853258400b67adaf2b58d9eef6
naga412 <nagalaxmijada5095@gmail.com>
2026-09-26 15:34:47 +0530

    userRoutes file updated

--- backend/src/modules/users/usersRoute.js
+++ backend/src/modules/users/usersRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const leadController = require("./usersController");
+const userController = require("./usersController");
 
-router.post('/create', leadController.createTeamLead);
-router.post('/login', leadController.loginTeamLead);
-router.get('/', leadController.getAllTeamLead);
-router.get('/count', leadController.getUserCount);
-router.get('/:id', leadController.getTeamLeadById);
-router.put('/:id', leadController.updateTeamLead);
+router.post('/create', userController.createUser);
+router.post('/login', userController.loginUser);
+router.get('/', userController.getAllUsers);
+router.get('/count', userController.getUserCount);
+router.get('/:id', userController.getUserById);
+router.put('/:id', userController.updateUser);
 module.exports = router;
